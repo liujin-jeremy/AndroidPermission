1fb85d4863097f342a12f2ece6d4fe2b33cc1fd8
liujin <wuxiongc@163.com>
2018-10-17 16:22:56 +0800

    update lib

--- permission/src/main/java/teck/threekilogram/permission/PermissionManager.java
+++ permission/src/main/java/teck/threekilogram/permission/PermissionManager.java
@@ -1,22 +1,19 @@
 package teck.threekilogram.permission;
 
-import static teck.threekilogram.permission.PermissionFunctions.ATOMIC_INTEGER;
-import static teck.threekilogram.permission.PermissionFunctions.KEY_LISTENER_INDEX;
-import static teck.threekilogram.permission.PermissionFunctions.KEY_PERMISSION;
-import static teck.threekilogram.permission.PermissionFunctions.REQUEST_CODE;
-import static teck.threekilogram.permission.PermissionFunctions.TEMP_LISTENER;
-
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager.LayoutParams;
+import android.widget.FrameLayout;
 
 /**
  * for request permission by fragment
@@ -26,9 +23,15 @@ import android.view.WindowManager.LayoutParams;
 public class PermissionManager extends DialogFragment {
 
       /**
+       * request permission code
+       */
+      private static final int REQUEST_CODE = 0b11101101;
+
+      /**
        * listener for result
        */
       private OnRequestPermissionResultListener mOnRequestPermissionResult;
+      private String                            mPermission;
 
       /**
        * request a permission
@@ -42,45 +45,86 @@ public class PermissionManager extends DialogFragment {
           String permission,
           OnRequestPermissionResultListener onRequestPermissionResult ) {
 
-            Bundle args = new Bundle();
-            args.putString(KEY_PERMISSION, permission);
-
-            int index = ATOMIC_INTEGER.getAndAdd(1);
-            TEMP_LISTENER.put(index, onRequestPermissionResult);
-            args.putInt(KEY_LISTENER_INDEX, index);
-
             PermissionManager fragment = new PermissionManager();
-            fragment.setArguments(args);
+            fragment.mPermission = permission;
+            fragment.mOnRequestPermissionResult = onRequestPermissionResult;
 
-            fragment.show(activity.getSupportFragmentManager(), fragment.toString());
+            fragment.show( activity.getSupportFragmentManager(), fragment.toString() );
+      }
+
+      @Nullable
+      @Override
+      public View onCreateView (
+          @NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+          @Nullable Bundle savedInstanceState ) {
+
+            FrameLayout frameLayout = new FrameLayout( getContext() );
+            frameLayout.setLayoutParams(
+                new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                ) );
+            return frameLayout;
+      }
+
+      @Override
+      public void onViewCreated ( @NonNull View view, @Nullable Bundle savedInstanceState ) {
+
+            super.onViewCreated( view, savedInstanceState );
+
+            /* make window not dark */
+
+            Window window = getDialog().getWindow();
+            if( window != null ) {
+
+                  LayoutParams params = window.getAttributes();
+                  params.dimAmount = 0f;
+                  window.setAttributes( params );
+            }
+
+            /* check permission */
+
+            if( checkPermission( getContext(), mPermission ) ) {
+
+                  if( mOnRequestPermissionResult != null ) {
+
+                        mOnRequestPermissionResult.onSuccess( mPermission );
+                  }
+
+                  dismiss();
+            } else {
+
+                  requestPermissions(
+                      new String[]{ mPermission }, REQUEST_CODE );
+            }
       }
 
       @Override
       public void onRequestPermissionsResult (
-          int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+          int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults ) {
 
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            super.onRequestPermissionsResult( requestCode, permissions, grantResults );
 
-            if(requestCode == REQUEST_CODE) {
+            if( requestCode == REQUEST_CODE ) {
 
-                  String permission = permissions[0];
-                  int grantResult = grantResults[0];
+                  String permission = permissions[ 0 ];
+                  int grantResult = grantResults[ 0 ];
 
-                  if(mOnRequestPermissionResult != null) {
+                  if( mOnRequestPermissionResult != null ) {
 
                         boolean result = grantResult == PackageManager.PERMISSION_GRANTED;
 
-                        if(result) {
+                        if( result ) {
 
-                              mOnRequestPermissionResult.onSuccess(permission);
+                              mOnRequestPermissionResult.onSuccess( permission );
                         } else {
 
-                              if(shouldShowRequestPermissionRationale(permission)) {
+                              if( shouldShowRequestPermissionRationale( permission ) ) {
 
                                     mOnRequestPermissionResult.onFailed( permission );
                               } else {
 
-                                    mOnRequestPermissionResult.onFinalDenied(permission);
+                                    mOnRequestPermissionResult.onFinalDenied( permission );
                               }
                         }
                   }
@@ -89,56 +133,14 @@ public class PermissionManager extends DialogFragment {
             }
       }
 
-      @Nullable
-      @Override
-      public View onCreateView (
-          @NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-          @Nullable Bundle savedInstanceState) {
+      /**
+       * check permission
+       *
+       * @return true have permission
+       */
+      public static boolean checkPermission ( Context context, String permissions ) {
 
-            return inflater.inflate(R.layout.empty, container, false);
-      }
-
-      @Override
-      public void onViewCreated (@NonNull View view, @Nullable Bundle savedInstanceState) {
-
-            super.onViewCreated(view, savedInstanceState);
-
-            /* make window not dark */
-
-            Window window = getDialog().getWindow();
-            if(window != null) {
-
-                  LayoutParams params = window.getAttributes();
-                  params.dimAmount = 0f;
-                  window.setAttributes(params);
-            }
-
-            /* get arguments */
-
-            String permission =
-                getArguments() != null ? getArguments().getString(KEY_PERMISSION) : null;
-
-            int index =
-                getArguments() != null ? getArguments().getInt(KEY_LISTENER_INDEX) : 0;
-
-            if(index > 0) {
-
-                  mOnRequestPermissionResult = TEMP_LISTENER.get(index);
-            }
-
-            /* check permission */
-
-            if(PermissionFunctions.checkPermission(getContext(), permission)) {
-
-                  if(mOnRequestPermissionResult != null) {
-
-                        mOnRequestPermissionResult.onSuccess(permission);
-                  }
-
-                  dismiss();
-            } else {
-
-                  requestPermissions(new String[]{permission}, PermissionFunctions.REQUEST_CODE);
-            }
+            return ContextCompat.checkSelfPermission( context, permissions )
+                == PackageManager.PERMISSION_GRANTED;
       }
 }
